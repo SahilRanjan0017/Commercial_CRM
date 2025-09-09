c6e0262dff8079604feac5f4d05f6d978d95c54d
Sahil Ranjan <sahil.ranjan@bricknbolt.com>
2025-09-09 20:16:32 +0000

    Change the Auth confirmation path If you have email confirmation turned

---
+++ src/app/auth/confirm/route.ts
@@ -0,0 +1,28 @@
+
+import { type EmailOtpType } from '@supabase/supabase-js'
+import { type NextRequest, NextResponse } from 'next/server'
+
+import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url)
+  const token_hash = searchParams.get('token_hash')
+  const type = searchParams.get('type') as EmailOtpType | null
+
+  if (token_hash && type) {
+    const supabase = createClient()
+
+    const { error } = await supabase.auth.verifyOtp({
+      type,
+      token_hash,
+    })
+    if (!error) {
+      // Redirect to the home page after successful verification
+      return NextResponse.redirect(new URL('/', request.url))
+    }
+  }
+
+  // Redirect to an error page if the token is invalid or missing
+  return NextResponse.redirect(new URL('/error', request.url))
+}
