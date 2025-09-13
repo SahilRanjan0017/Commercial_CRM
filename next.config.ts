ed19381a441bdde69c0ec7922364fb6f27e6fe9d
Sahil Ranjan <sahil.ranjan@bricknbolt.com>
2025-09-13 02:29:17 +0000

    There's a syntax error in your next.config.ts file! You have an untermin

--- next.config.ts
+++ next.config.ts
@@ -36,7 +36,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,
+        protocol: 'https',
         hostname: 'i.postimg.cc',
         port: '',
         pathname: '/**',
